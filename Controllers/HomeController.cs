76b962d00179eda9e3a31443c0034d7d91abd337
jpjayapriya <jayapriyajp68@gmail.com>
2024-07-10 23:10:45 +0530

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using WebApplication1.Models;
+using System.Collections.Generic;
+using Microsoft.Extensions.Logging;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            var products = new List<Product>
+        {
+            new Product { Id = 1, Name = "Product 1", Price = 10},
+            new Product { Id = 2, Name = "Product 2", Price = 20 },
+            new Product { Id = 3, Name = "Product 3", Price = 30 }
+        };
+
+            return View(products);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
